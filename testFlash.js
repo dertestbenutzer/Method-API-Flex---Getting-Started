5236e10fa45bad864e87d7a55780f8ce30877e69
dertestbenutzer <der.test.benutzer@googlemail.com>
2011-04-19 05:40:57 -0700

    Edited testFlash.js via GitHub

--- testFlash.js
+++ testFlash.js
@@ -1,5 +1,5 @@
 (function () {
     /* Change APPLICATION_KEY to the one specified in your bundle.xml and
 	 * URL_TO_SWF to the URL of the swf file (without the extension!) */
-    streamwork.flash.registerModule("com.sap.mueller.ruediger.flash.gettingstarted", "https://github.com/dertestbenutzer/Method-API-Flex---Getting-Started/raw/master/TestFlashMethod");
+    streamwork.flash.registerModule("com.sap.mueller.ruediger.flash.gettingstarted", "http://swgae.googlecode.com/svn/TestFlashMethod");
 })();
